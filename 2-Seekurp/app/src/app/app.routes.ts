f4ebc668305a809b0b1a04d9ff73521c31f5465d
Catppuccino <tarduvias@gmail.com>
2024-07-09 11:54:12 -0600

    [Add] Home's page seekurp

--- 2-Seekurp/app/src/app/app.routes.ts
+++ 2-Seekurp/app/src/app/app.routes.ts
@@ -1,4 +1,5 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 import { PipeComponent } from './pipe/pipe.component';
 import { AuthGuard } from './auth.guard';
 import { PipelineComponent } from './pipeline/pipeline.component';
@@ -7,6 +8,8 @@ import { ExplorerComponent } from './explorer/explorer.component';
 import { NotFoundComponent } from './notfound/notfound.component';
 
 export const routes: Routes = [
+    { path: '', redirectTo: '/home', pathMatch: 'full' },
+    { path: 'home', component: HomeComponent },
     { path: 'pipe', component: PipeComponent },
     { path: 'pipeline', component: PipelineComponent, canActivate: [AuthGuard] },
     { path: 'error', component: ErrorComponent },
