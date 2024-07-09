091558107e8268e523d8f2b6c41d6b0721d6175e
Catppuccino <tarduvias@gmail.com>
2024-07-09 11:31:43 -0600

    [Add] Not found seekurp

--- 2-Seekurp/app/src/app/app.routes.ts
+++ 2-Seekurp/app/src/app/app.routes.ts
@@ -3,9 +3,13 @@ import { PipeComponent } from './pipe/pipe.component';
 import { AuthGuard } from './auth.guard';
 import { PipelineComponent } from './pipeline/pipeline.component';
 import { ErrorComponent } from './error/error.component';
+import { ExplorerComponent } from './explorer/explorer.component';
+import { NotFoundComponent } from './notfound/notfound.component';
 
 export const routes: Routes = [
     { path: 'pipe', component: PipeComponent },
     { path: 'pipeline', component: PipelineComponent, canActivate: [AuthGuard] },
-    { path: 'error', component: ErrorComponent }
+    { path: 'error', component: ErrorComponent },
+    { path: 'explorer', component: ExplorerComponent },
+    { path: '**', component: NotFoundComponent }
 ];
